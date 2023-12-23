ed42fc217da85b9dccff332f92b8e94d27f86fe9
anmay12sri <103562464+anmay12sri@users.noreply.github.com>
2023-12-24 00:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (12.98%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -3,14 +3,15 @@ class Solution {
         for(int i=digits.length-1;i>=0;i--){
             if(digits[i]<9){
                 digits[i]++;
-                return digits;//4,3,2,
+                return digits;
             }else if(digits[i]==9){
-                digits[i]=0;
+                 digits[i]=0;
             }
         }
-              int dig[]=new int[digits.length+1];
-              dig[0]=1;
-              return dig;
+
+        int newdig[]=new int[digits.length+1];
+        newdig[0]=1;
+        return newdig;
             
         
         
