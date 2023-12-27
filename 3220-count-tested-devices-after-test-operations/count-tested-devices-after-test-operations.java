e853b18a8adef26e7658dce2be4d4facf1476b2b
anmay12sri <103562464+anmay12sri@users.noreply.github.com>
2023-12-28 00:39:34 +0530

    Time: 1 ms (79.41%) | Memory: 42.8 MB (92.76%) - LeetSync

--- 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
@@ -1,11 +1,11 @@
 class Solution {
     public int countTestedDevices(int[] nums) {
-        int x=0;//subtract krne ke liye
         int ans=0;
+        int x=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]-x>0){// 1>0 
-              x++;//1
-              ans++;//1
+            if(nums[i]-x>0){
+                x++;
+                ans++;
             }
         }
         return ans;
