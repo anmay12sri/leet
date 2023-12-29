244e094350c9f71df9b56020a0c27ace916a5f00
anmay12sri <103562464+anmay12sri@users.noreply.github.com>
2023-12-29 21:05:34 +0530

    Time: 1 ms (46.85%) | Memory: 42.9 MB (80.44%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -7,6 +7,7 @@ class Solution {
             if(nums[i]<min)min=nums[i];
             if(nums[i]>max)max=nums[i];
         }
+
         for(int i=2;i<=min && i<=max;i++)
 
             if((max%i==0)&&(min%i==0))
