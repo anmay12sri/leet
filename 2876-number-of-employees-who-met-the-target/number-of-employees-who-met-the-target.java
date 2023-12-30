268efe31fecd24a7b52acbc45c7bd67a77e26bc2
anmay12sri <103562464+anmay12sri@users.noreply.github.com>
2023-12-30 22:45:28 +0530

    Time: 2 ms (5.02%) | Memory: 42.1 MB (10.83%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
@@ -1,11 +1,9 @@
 class Solution {
-    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int count=0;
-        for(int i=0;i<hours.length;i++){
-            if(hours[i]>=target){
-                count++;
-            }
-        }
-        return count;
-    }
-}
+        public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+                int cnt = 0; 
+                        for(var h : hours){
+                                    if(h >= target)cnt++;
+                                            }
+                                                    return cnt;
+                                                        }
+                                                        }
