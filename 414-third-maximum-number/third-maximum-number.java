df7504a8420a7e4d935c21acc532a8e7b0203509
anmay12sri <103562464+anmay12sri@users.noreply.github.com>
2024-01-06 21:52:26 +0530

    Time: 5 ms (33.14%) | Memory: 44.6 MB (5.47%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -4,10 +4,10 @@ class Solution {
         Arrays.sort(nums);//1,2,2,3  1->target
        for(int i=0;i<nums.length-1;i++){
            if(nums[i]!=nums[i+1]){
-             arr.add(nums[i]);
+             arr.add(nums[i]);//1,2
            }
        }
-       arr.add(nums[nums.length-1]);//last wala remaining tha
+       arr.add(nums[nums.length-1]);//last wala remaining tha 1,2,3
 
        if(arr.size()<3){
            return arr.get(arr.size()-1);
