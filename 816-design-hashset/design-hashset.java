e73c6ec2c923cab5ba29ac436594527e127d7a94
anmay12sri <103562464+anmay12sri@users.noreply.github.com>
2024-01-10 22:47:49 +0530

    Time: 12 ms (86.58%) | Memory: 49.5 MB (63.15%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -5,15 +5,15 @@ class MyHashSet {
     }
     
     public void add(int i) {
-       arr[i]=true; 
+       arr[i]=true; //true ho toh add hoga
     }
     
     public void remove(int i) {
-        arr[i]=false;
+        arr[i]=false; //false wala remove ke liye
     }
     
     public boolean contains(int i) {
-        return arr[i];
+        return arr[i];//end mein pura arr return
     }
 }
 
