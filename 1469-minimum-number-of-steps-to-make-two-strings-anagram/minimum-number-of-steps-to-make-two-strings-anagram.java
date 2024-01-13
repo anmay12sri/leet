23ca4b3d62ee3c3b4962aa4e3863cd36e2ddab02
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-01-13 23:14:50 +0530

    Time: 8 ms (76.51%) | Memory: 45.3 MB (24.18%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,21 +1,23 @@
 class Solution {
     public int minSteps(String s, String t) {
+        //let's begin
+        //take two arrays of length 26
         int arr_s[]=new int[26];
         int arr_t[]=new int[26];
         int count=0;
 
+        //now we have to count the occurances of alphabets
         for(int i=0;i<s.length();i++){
-            arr_s[s.charAt(i)-'a']++;//bab 1 2
-            arr_t[t.charAt(i)-'a']++;// aba 2 1
-
-        
+            arr_s[s.charAt(i)-'a']++;//bab b=98 ascii a=97 so b=1 and ++ so b=2 a=1
+            arr_t[t.charAt(i)-'a']++;//same for the t also
         }
 
         for(int i=0;i<26;i++){
-            if(arr_s[i]>arr_t[i]){
-               count+=arr_s[i]-arr_t[i]; 
+            if(arr_s[i]>arr_t[i]){//we will only subtract from the s
+                count+=arr_s[i]-arr_t[i];
             }
         }
         return count;
+        
     }
 }
