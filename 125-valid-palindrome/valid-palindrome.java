891d134c743ad9995a5a796d76c4774ab0ab68bb
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-01-13 23:53:03 +0530

    Time: 15 ms (37.71%) | Memory: 44.7 MB (25.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,6 +4,7 @@ class Solution {
         s=s.toLowerCase().replaceAll("[^a-z0-9]","");
         int i=0;
         int j=s.length()-1;
+
         while(i<=j){
             if(s.charAt(i)!=s.charAt(j)){
                 return false;
