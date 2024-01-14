7effffdb2f10f2ba837a00cf2829c371d6a900b1
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-01-14 15:26:03 +0530

    Time: 12 ms (69.52%) | Memory: 45.7 MB (14.80%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -17,12 +17,7 @@ class Solution {
       Arrays.sort(arr1);
       Arrays.sort(arr2);
 
-      for(int i=0;i<26;i++){
-          if(arr1[i]!=arr2[i]){
-              return false;
-          }
-      }
-      return true;
+      return Arrays.equals(arr1,arr2);
 
 
         
