039d679f660d96ae7715554f18c3ea84b34962e9
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-01-19 00:46:09 +0530

    Time: 2 ms (77.46%) | Memory: 46 MB (32.38%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,8 +1,8 @@
 class Solution {
     public void moveZeroes(int[] arr) {
         int j=0;
+
         for(int i=0;i<arr.length;i++){
-            
             if(arr[i]!=0){
                 int temp=arr[i];
                 arr[i]=arr[j];
@@ -10,5 +10,7 @@ class Solution {
                 j++;
             }
         }
+        
+        
     }
 }
