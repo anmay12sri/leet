5a03f8198fa5148c9b241a21ae0ef73556461787
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-01-20 00:05:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (28.41%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length())return false;
+
+        StringBuilder str=new StringBuilder(goal);
+
+        str=str.append(str);
+
+        if(str.indexOf(s)!=-1){
+            return true;
+        }
+        return false;
+    }
+}
