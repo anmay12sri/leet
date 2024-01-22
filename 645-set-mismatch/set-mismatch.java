19c9b11fe4fa361d3c16069964e5848b0927749d
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-01-22 14:47:30 +0530

    Time: 11 ms (23.97%) | Memory: 45.8 MB (17.33%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int n=nums.length;
-        int actual_sum=(n*(n+1))/2;
-        int arr_sum=0;
-        int uni_sum=0;
-
-        Set<Integer>set=new HashSet<>();
-        for(int x:nums){
-            arr_sum+=x;
-        }
-        for(int s:nums){
-            set.add(s);
-        }
-        for(int y:set){
-            uni_sum+=y;
-        }
-        int duplicate=arr_sum-uni_sum;
-        int notpresent=actual_sum-uni_sum;
-
-
-        return new int[]{duplicate,notpresent};
+        int total_sum=n*(n+1)/2;//10
+        int actual_sum=0;
+        int set_sum=0;
         
+          Set<Integer>set=new HashSet<>();
+        for(int i:nums){
+            actual_sum+=i;//9
+        }
+        for(int x:nums){
+            set.add(x);//1,2,4
+        }
+        for(int s:set){
+            set_sum+=s;//7
+        }
+
+        int duplicate=actual_sum-set_sum;
+        int change=total_sum-set_sum;
+
+      return new int[]{duplicate,change};
+         
+
     }
 }
