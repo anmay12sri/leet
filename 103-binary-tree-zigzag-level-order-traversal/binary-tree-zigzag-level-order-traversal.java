dbe3542b2a80e2483cc35dee7a443a60b0f28d8f
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-02-19 13:53:49 +0530

    Time: 1 ms (78.69%) | Memory: 42.1 MB (63.66%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -18,29 +18,26 @@ class Solution {
         List<List<Integer>>list=new ArrayList<>();
         Queue<TreeNode>q=new LinkedList<>();
         if(root==null)return list;
-        q.offer(root);
         boolean flag=true;
+        q.offer(root);
         while(!q.isEmpty()){
-        int n=q.size();
-
-        List<Integer>lis=new ArrayList<>(n);
-        for(int i=0;i<n;i++){
-            if(q.peek()
-               .left!=null){
-                q.offer(q.peek().left);
+            int n=q.size();
+            List<Integer>lis=new ArrayList<>(n);
+            for(int i=0;i<n;i++){
+                if(q.peek().left!=null){
+                    q.offer(q.peek().left);
+                }
+                if(q.peek().right!=null){
+                    q.offer(q.peek().right);
+                }
+                if(flag){
+                    lis.add(q.poll().val);
+                }else{
+                    lis.add(0,q.poll().val);
+                }
             }
-            if(q.peek().right!=null){
-                q.offer(q.peek().right);
-            }
-            if(flag){
-                lis.add(q.poll().val);
-            }else{
-                lis.add(0,q.poll().val);
-            }
-        }
-         flag=!flag;
-
-         list.add(lis);
+            flag=!flag;
+            list.add(lis);
         }
         return list;
     }
