a1e132937b6cdcee7a7d2cd5c7cb56cb5b84527f
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-02-25 12:58:22 +0530

    Time: 538 ms (34.34%) | Memory: 40.9 MB (95.44%) - LeetSync

---
+++ 494-target-sum/target-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+       return sum(nums,0,0,target);  
+    }
+    public static int sum(int nums[],int i,int curr,int target){
+        if(i==nums.length){
+            if(curr==target){
+                return 1;
+            }
+            return 0;
+        }
+        int left=sum(nums,i+1,curr+nums[i],target);
+        int right=sum(nums,i+1,curr-nums[i],target);
+        return left+right;
+    }
+}
