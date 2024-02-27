ab973a08a572f65c8f08df315f7a9367e6fbd46a
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-02-28 00:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (74.67%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -9,33 +9,33 @@ class Solution {
                 break;
             }
         }
-            if(ind1==-1){
-                reverse(nums,0);
-            }else{
-                for(int i=n-1;i>=0;i--){
-                    if(nums[ind1]<nums[i]){
-                       ind2=i;
-                       break;
-                    }
-                }
-            
+        if(ind1==-1){
+        reverse(nums,0);
+        }
+        else{
+        for(int i=n-1;i>=0;i--){
+            if(nums[ind1]<nums[i]){
+                ind2=i;
+                break;
+            }
+        }
             swap(nums,ind1,ind2);
             reverse(nums,ind1+1);
-            }
 
+        }
     }
-    public void swap(int nums[],int l,int r){
-        int temp=nums[l];
-        nums[l]=nums[r];
-        nums[r]=temp;
+    public void swap(int nums[],int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
     public void reverse(int nums[],int start){
-        int l=start;
-        int r=nums.length-1;
-        while(l<r){
-            swap(nums,l,r);
-            l++;
-            r--;
+        int i=start;
+        int j=nums.length-1;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
         }
     }
 }
