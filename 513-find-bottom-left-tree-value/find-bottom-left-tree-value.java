96d1d26fc3a46f6910f35a3032ec43dad7e70753
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-02-28 13:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (73.67%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -15,16 +15,17 @@
  */
 class Solution {
     int maxheight=Integer.MIN_VALUE;
-        int ans=0;
+    int ans=0;
+
     public int findBottomLeftValue(TreeNode root) {
-         bottom(root,0);
-         return ans;
+       bottom(root,0);
+       return ans; 
     }
     public void bottom(TreeNode root,int height){
         if(root==null)return;
         if(height>maxheight){
-            maxheight=height;
-            ans=root.val;
+         maxheight=height;
+         ans=root.val;
         }
         bottom(root.left,height+1);
         bottom(root.right,height+1);
