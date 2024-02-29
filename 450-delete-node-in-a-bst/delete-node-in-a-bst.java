30a1820856f62d0c99544b347c76d97d1c724a1c
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-03-01 01:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (78.67%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -15,42 +15,37 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null)return null;
+       if(root==null)return null;
 
-        if(root.val==key)return helper(root);
-        TreeNode temp=root;
+       if(root.val==key)return helper(root);
+       TreeNode temp=root;
 
-        while(root!=null){
-            if(root.val>key){
-                if(root.left!=null && root.left.val==key){
-                    root.left= helper(root.left);
-                    break;
-                }else{
-                    root=root.left;
-                }
-            }else{
-                if(root.right!=null && root.right.val==key){
-                    root.right= helper(root.right);
-                    break;
-                }else{
-                    root=root.right;
-                }
-            }
-        }
-        return temp;
-
-
+       while(root!=null){
+           if(root.val>key){
+               if(root.left!=null && root.left.val==key){
+                   root.left=helper(root.left);
+                   break;
+               }else{
+                   root=root.left;
+               }
+           }else{
+               if(root.right!=null && root.right.val==key){
+                   root.right=helper(root.right);
+                   break;
+               }else{
+                   root=root.right;
+               }
+           }
+       }
+       return temp; 
     }
     public TreeNode helper(TreeNode root){
-        if(root.left==null){
-            return root.right;
-        }else if(root.right==null){
-            return root.left;
-        }
+        if(root.left==null)return root.right;
+        else if(root.right==null)return root.left;
 
         TreeNode rightchild=root.right;
-        TreeNode lastright=LastRight(root.left);
-        lastright.right=rightchild;
+        TreeNode Lastright=LastRight(root.left);
+        Lastright.right=rightchild;
         return root.left;
     }
     public TreeNode LastRight(TreeNode root){
