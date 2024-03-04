b2a3735d43bfc38de9767257257e4399cb6a49ff
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-03-04 14:28:52 +0530

    Time: 3 ms (37.26%) | Memory: 42.1 MB (98.41%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.java
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -1,28 +1,27 @@
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
-        Arrays.sort(tokens);
         int i=0;
         int j=tokens.length-1;
+        Arrays.sort(tokens);
         int score=0;
         int ans=0;
 
-       while(i<=j){
-           if(tokens[i]<=power){
-               score+=1;
-               if(ans<score){
-                   ans=score;
-               }
-               power-=tokens[i];
-               i++;
-           }
-           else if(score>0){
-               power+=tokens[j];
-               score-=1;
-               j--;
-           }else{
-               break;
-           }
-       }
-       return ans;
+        while(i<=j){
+         if(tokens[i]<=power){
+             score+=1;
+             if(ans<score){
+                 ans=score;
+             }
+             power-=tokens[i];
+             i++;
+         }else if(score>0){
+             power+=tokens[j];
+             score-=1;
+             j--;
+         }else{
+             break;
+         }
+        }
+        return ans;
     }
 }
