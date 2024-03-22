3ab4f82f77b08a65a78f9c6fc5a8396e49f0fe3a
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-03-22 11:29:22 +0530

    Time: 4 ms (82.66%) | Memory: 65.3 MB (59.35%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,28 +10,28 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        
-        while(fast.next!=null && fast.next.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        ListNode newhead=reverse(slow.next);
-        ListNode first=head;
-        ListNode second=newhead;
+       ListNode slow=head;
+       ListNode fast=head;
+       while(fast.next!=null && fast.next.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+       } 
+       ListNode newhead=reverse(slow.next);
 
-        while(second!=null){
-            if(first.val!=second.val){
-                reverse(newhead);
-                return false;
-            }
-            first=first.next;
-            second=second.next;
-            
+       ListNode first=head;
+       ListNode second=newhead;
+
+       while(second!=null){
+        if(first.val!=second.val){
+            reverse(newhead);
+            return false;
         }
-        reverse(newhead);
-        return true;
+        first=first.next;
+        second=second.next;
+        
+       }
+       reverse(newhead);
+       return true;
     }
     public static ListNode reverse(ListNode head){
         ListNode prev=null;
