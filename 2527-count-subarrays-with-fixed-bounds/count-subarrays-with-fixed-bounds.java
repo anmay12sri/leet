eb4e49667c5a26c3d2f49c54264d9c045d7f86c6
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-03-31 16:00:36 +0530

    Time: 8 ms (51.62%) | Memory: 59.7 MB (50.18%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.java
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.java
@@ -1,36 +1,26 @@
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-       long ans=0;
-     int minkposition=-1;
-     int maxkposition=-1;
-     int culprit =-1;
-      
+        int minKpos=-1;
+        int maxKpos=-1;
+        int culprit=-1;
+        long ans=0;
 
-     for(int i=0;i<nums.length;i++){
-        if(nums[i]<minK || nums[i]>maxK){
-            culprit=i;
-        }
-             if(nums[i]==minK){
-                minkposition=i;
-             }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<minK || nums[i]>maxK){
+                culprit=i;
+            }
+            if(nums[i]==minK){
+                minKpos=i;
+            }
+            if(nums[i]==maxK){
+                maxKpos=i;
+            }
 
-             if(nums[i]==maxK){
-            maxkposition=i;
-             }
-
-
-
-            long smaller=Math.min(minkposition,maxkposition);
-
+            long smaller=Math.min(minKpos,maxKpos);
             long temp=smaller-culprit;
 
             ans+=(temp<=0)?0:temp;
-
-             
-    }
-         
-     
-   return ans;
-        
+        }
+        return ans;
     }
 }
