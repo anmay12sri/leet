dde8922ca0da2d904ac0aeee33442688bc995834
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-04-04 13:06:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (72.26%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -3,10 +3,11 @@ class Solution {
         List<Integer>list=new ArrayList<>();
         int m=matrix.length;
         int n=matrix[0].length;
-        int left=0;
-        int right=n-1;
+
         int top=0;
         int bottom=m-1;
+        int left=0;
+        int right=n-1;
 
         while(top<=bottom && left<=right){
             for(int i=left;i<=right;i++){
@@ -22,13 +23,15 @@ class Solution {
                     list.add(matrix[bottom][i]);
                 }
                 bottom--;
-            }
+            
             if(left<=right){
                 for(int i=bottom;i>=top;i--){
                     list.add(matrix[i][left]);
                 }
                 left++;
-            }
+            }    
+            
+        }
         }
         return list;
     }
