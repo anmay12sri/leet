aba67fc988499c89172ab1b16bb110910d65633a
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-04-05 15:12:11 +0530

    Time: 3 ms (61.07%) | Memory: 42.2 MB (57.36%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,15 +1,16 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character>st=new Stack<>();
+      Stack<Character>st=new Stack<>();
 
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-
-            if(st.size()==0)st.push(c);
-        else{
-            if(Math.abs(st.peek()-c)==32)st.pop();
-            else st.push(c);
-        }
+              char c=s.charAt(i);
+                if(st.size()==0)st.push(c);
+                else{
+                    if(Math.abs(st.peek()-c)==32)st.pop();
+                    else{
+                        st.push(c);
+                    }
+                }
         }
 
         String ans="";
