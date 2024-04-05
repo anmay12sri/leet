1ffc79240af37d3e096df06112b0641658ae0db5
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-04-05 22:25:06 +0530

    Time: 42 ms (69.97%) | Memory: 49.3 MB (15.39%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -3,16 +3,14 @@ class Solution {
         List<Character>vowellist=new ArrayList<>();
         List<Integer>vowelposition=new ArrayList<>();
         for(int i=0;i<s.length();i++){
-             char c=s.charAt(i);
-             if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U'||c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+            char c=s.charAt(i);
+            if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U'||c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
                 vowellist.add(c);
                 vowelposition.add(i);
-             }
+            }
         }
         Collections.sort(vowellist);
-        
-        char []res=s.toCharArray();
-
+        char res[]=s.toCharArray();
         for(int i=0;i<vowelposition.size();i++){
             res[vowelposition.get(i)]=vowellist.get(i);
         }
