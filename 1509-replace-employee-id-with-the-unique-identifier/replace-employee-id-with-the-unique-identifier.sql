b27f812d6d74e49a191695021ddc8c20cfb0f9b6
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-04-09 01:00:48 +0530

    Time: 2219 ms (58.38%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-SELECT Employees.name,EmployeeUNI.unique_id
-FROM Employees
-LEFT JOIN EmployeeUNI  on Employees.id=EmployeeUNI.id;
+ Select Employees.name,EmployeeUNI.unique_id 
+ from Employees
+  left join EmployeeUNI
+--  order by Employees.id desc
+ on Employees.id=EmployeeUNI.id 
+ order by Employees.id ;
