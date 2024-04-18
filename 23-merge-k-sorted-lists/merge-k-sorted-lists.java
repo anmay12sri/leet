e761d0d11b249592e5de5f3dfd80cccae848122b
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-04-18 22:16:52 +0530

    Time: 172 ms (5.06%) | Memory: 44.8 MB (14.68%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,29 +9,23 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwolinkedlist(ListNode l1,ListNode l2){
-        if(l1==null){
-            return l2;
-        }
-        if(l2==null){
-            return l1;
-        }
+    public ListNode mergetwo(ListNode l1,ListNode l2){
+        if(l1==null)return l2;
+        if(l2==null)return l1;
+
         if(l1.val<l2.val){
-          l1.next=mergeTwolinkedlist(l1.next,l2);
-          return l1;
+            l1.next=mergetwo(l1.next,l2);
+            return l1;
         }else{
-            l2.next=mergeTwolinkedlist(l2.next,l1);
-            return l2;
+             l2.next=mergetwo(l2.next,l1);
+             return l2;
         }
     }
-
     public ListNode mergeKLists(ListNode[] lists) {
-       ListNode ans=null;
-
-       for(int i=0;i<lists.length;i++){
-        ans=mergeTwolinkedlist(ans,lists[i]);
-       }
-       return ans;
-        
+        ListNode ans=null;
+        for(int i=0;i<lists.length;i++){
+            ans=mergetwo(ans,lists[i]);
+        }
+        return ans;
     }
 }
