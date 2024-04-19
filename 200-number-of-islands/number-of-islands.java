06c9cced39ef262487812f2f6fac5dab0e2fd380
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-04-19 15:36:34 +0530

    Time: 3 ms (86.63%) | Memory: 49 MB (86.32%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,8 +1,6 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        //  if (grid == null || grid.length == 0) {
-        //     return 0;
-        // }
+        
         int count=0;
        for(int i=0;i<grid.length;i++){
         for(int j=0;j<grid[0].length;j++){
@@ -18,7 +16,7 @@ class Solution {
         if(i<0||i>=grid.length||j<0||j>=grid[0].length||grid[i][j]!='1'){
             return;
         }
-        grid[i][j]='0';
+        grid[i][j]='0';//visited mark kr diya
         dfs(grid,i-1,j);
         dfs(grid,i+1,j);
         dfs(grid,i,j+1);
