7507a60f5316ebeeb7c9af87a375ee2b93f17ca2
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-04-24 16:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 57.1 MB (77.12%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,8 +1,6 @@
 class Solution {
-
-    public static void arr(int nums[],int s,int e){
-        
-        while(s<e){
+    public void rotatee(int nums[],int s,int e){
+        while(s<=e){
             int temp=nums[s];
             nums[s]=nums[e];
             nums[e]=temp;
@@ -12,8 +10,8 @@ class Solution {
     }
     public void rotate(int[] nums, int k) {
         k=k%nums.length;
-        arr(nums,0,nums.length-1);
-        arr(nums,0,k-1);
-        arr(nums,k,nums.length-1);
+        rotatee(nums,0,nums.length-1);
+        rotatee(nums,0,k-1);
+        rotatee(nums,k,nums.length-1);
     }
 }
