8f1675793cd2d75146aff1698de8d1f51dd7918e
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-04-27 00:24:50 +0530

    Time: 31 ms (62.59%) | Memory: 51 MB (53.06%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -2,11 +2,10 @@ class Solution {
     public int minFallingPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-
         int dp[][]=new int[m][n];
 
         for(int j=0;j<n;j++){
-            dp[0][j]=grid[0][j];//taking first value
+            dp[0][j]=grid[0][j];
         }
         for(int i=1;i<m;i++){
             for(int j=0;j<n;j++){
@@ -14,17 +13,17 @@ class Solution {
                 int prev=Integer.MAX_VALUE;
                 for(int k=0;k<n;k++){
                     if(k!=j){
-                    prev=Math.min(prev,dp[i-1][k]);
+                        prev=Math.min(prev,dp[i-1][k]);
                     }
                 }
                 dp[i][j]+=prev;
+
             }
         }
-        int min_sum=Integer.MAX_VALUE;
+        int sum=Integer.MAX_VALUE;
         for(int j=0;j<n;j++){
-            min_sum=Math.min(min_sum,dp[n-1][j]);
+         sum=Math.min(sum,dp[n-1][j]);
         }
-        
-   return min_sum;
+       return sum;
     }
 }
