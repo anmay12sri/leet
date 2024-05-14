dabc86b0fdfbb148a94e68bc4b740f9b4b88397a
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-05-14 16:37:55 +0530

    Time: 65 ms (66.56%) | Memory: 41 MB (56.84%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,43 +1,30 @@
 class Solution {
     public int getMaximumGold(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int maxGold = 0;
-
-        
-        class Helper {
-            public int dfs(int row, int col) {
-                if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == 0) {
-                    return 0;
-                }
-
-                int currentGold = grid[row][col];
-                grid[row][col] = 0; 
-
-                
-                int d = dfs(row + 1, col);
-                int u = dfs(row - 1, col);
-                int r = dfs(row, col + 1);
-                int l = dfs(row, col - 1);
-
-              
-                grid[row][col] = currentGold;
-
-                return currentGold + Math.max(Math.max(d, u), Math.max(r, l));
+        int row=grid.length,col=grid[0].length;
+        int visited[][]=new int[row][col];
+        int ans=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]!=0) ans=Math.max(find_max(grid,visited,i,j),ans);
             }
         }
-
-        Helper helper = new Helper();
-
-        
-        for (int row = 0; row < m; ++row) {
-            for (int col = 0; col < n; ++col) {
-                if (grid[row][col] > 0) {
-                    maxGold = Math.max(maxGold, helper.dfs(row, col));
-                }
-            }
-        }
-
-        return maxGold;
+        return ans;
     }
+    public static int find_max(int[][] arr,int[][] visited,int i,int j){
+        if(i<0||j<0||i>=arr.length||j>=arr[0].length||visited[i][j]==1||arr[i][j]==0) return -1000000;
+
+        visited[i][j]=1;
+        int a=find_max(arr,visited,i+1,j);
+        if(a<0) a=0;
+        int b=find_max(arr,visited,i-1,j);
+        if(b<0) b=0;
+        int c=find_max(arr,visited,i,j+1);
+        if(c<0) c=0;
+        int d=find_max(arr,visited,i,j-1);
+        if(d<0) d=0;
+        visited[i][j]=0;
+        int max=Math.max(Math.max(a,b),Math.max(c,d));
+        return max+arr[i][j];
+    }
+
 }
