b182cbde37777b03ed33aacdca27ab464ba25e1d
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-05-17 00:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (99.67%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.java
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.java
@@ -16,10 +16,7 @@
 class Solution {
     public boolean evaluateTree(TreeNode root) {
         if (root.val <= 1)
-            return root.val == 1 ? true : false;
-
-        // int a=evaluateTree(root.left);
-        // int b=evaluateTree(root.right);
+            return root.val == 1;
 
         if(root.val==2){
             return evaluateTree(root.left)||evaluateTree(root.right);
