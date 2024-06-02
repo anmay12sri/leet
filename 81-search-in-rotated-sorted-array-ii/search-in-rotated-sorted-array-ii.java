7b917ff0a63f2b46b7d35838bd0221d29917d47f
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-06-03 00:39:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (42.08%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,35 +1,34 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int l=0;
-        int h=nums.length-1;
-
-        while(l<=h){
-            int mid=l+(h-l)/2;
-
-            if(nums[mid]==target)return true;
-
-            if(nums[l]==nums[mid]  && nums[mid]==nums[h]){
-                 l++;
-                 h--;
-                 continue;
+        if(nums.length <2 && target == nums[0]) return true; 
+        if(nums.length <2 && target != nums[0]) return false; 
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target || nums[low]==target || nums[high]==target){
+                return true;
+            }else if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;
             }
-            if(nums[l]<=nums[mid]){
-            if(nums[l]<=target && target<=nums[mid]){
-                    h=mid-1;
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target <= nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
             }else{
-                 l=mid+1;
-            }
-            }
-            else{
-                if(nums[mid+1]<=target && target<=nums[h]){
-                    l=mid+1;
-                }else{
-                    h=mid-1;
+                if(nums[mid]<= target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
                 }
             }
         }
-
-            return false;
-        
+        return false;
     }
 }
