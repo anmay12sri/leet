644dcabc9ed181fd97e8140f395b78c1d49749c5
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-06-06 00:07:05 +0530

    Time: 5 ms (44.31%) | Memory: 42.8 MB (73.01%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,25 +1,27 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        int[] finalFreq = new int[26];
-        Arrays.fill(finalFreq, Integer.MAX_VALUE);
-
-        for (String word : words) {
-            int[] curFreq = new int[26];
-            for (char c : word.toCharArray()) {
-                curFreq[c - 'a']++;
+        int arr[]=new int[26];
+        for(int i=0;i<words[0].length();i++){
+            arr[words[0].charAt(i)-'a']++;
+        }
+        for(int i=1;i<words.length;i++){
+            int freq[]=new int[26];
+            for(int k=0;k<words[i].length();k++){
+                freq[words[i].charAt(k)-'a']++;
             }
-            for (int i = 0; i < 26; i++) {
-                finalFreq[i] = Math.min(finalFreq[i], curFreq[i]);
+            for(int l=0;l<26;l++){
+                arr[l]=Math.min(arr[l],freq[l]);
             }
         }
-
-        List<String> ans = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
-            while (finalFreq[i] > 0) {
-                ans.add(String.valueOf((char) (i + 'a')));
-                finalFreq[i]--;
+        List<String>list=new ArrayList<>();
+        for(int l=0;l<26;l++){
+            if(arr[l]>0){
+                Character ch=(char)(l+'a');
+                for(int i=0;i<arr[l];i++){
+                    list.add(""+ ch);
+                }
             }
         }
-        return ans;
+        return list;
     }
 }
