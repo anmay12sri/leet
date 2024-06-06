8be380ed1f929c582fd0b3977877a21f25d798a5
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-06-06 23:25:19 +0530

    Time: 6 ms (85.16%) | Memory: 57.5 MB (43.75%) - LeetSync

--- 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.java
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.java
@@ -10,28 +10,28 @@ class Solution {
 
 
         }
-        int prevEven=0;
-        int prevOdd=0;
+        int prevEven=0;//2 8
+        int prevOdd=0;//1
         int count=0;
 
         for(int i=0;i<nums.length;i++){
             if(i%2==0){
-                e-=nums[i];
-                if(e+prevOdd==o+prevEven){
+                e-=nums[i];//6 0
+                if(e+prevOdd==o+prevEven){//6==5 1!=6
                     count++;
                 }
-                    prevEven+=nums[i];
+                    prevEven+=nums[i];//
                 
             }else{
-                o-=nums[i];
-                if(o+prevEven==e+prevOdd){
-                    count++;
+                o-=nums[i];//4 0
+                if(o+prevEven==e+prevOdd){//4+2==6+0 8==6+1 no
+                    count++;//1
                 } 
-                    prevOdd+=nums[i];
+                    prevOdd+=nums[i];//1 5
                 
             }
         }
-        return count;
+        return count;//1
 
     }
 }
