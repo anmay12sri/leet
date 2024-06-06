d9163f1bca616137d73be8df968a8be986045e75
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-06-06 19:15:13 +0530

    Time: 59 ms (23.31%) | Memory: 45.3 MB (72.20%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -2,22 +2,23 @@ class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         if(hand.length%groupSize!=0){
             return false;
-        }else{
-            PriorityQueue<Integer>pq=new PriorityQueue<>();
-        for(int i=0;i<hand.length;i++){
-          pq.offer(hand[i]);
         }
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        
+        for(int i=0;i<hand.length;i++){
+            pq.offer(hand[i]);
+        }
+
         while(!pq.isEmpty()){
             int head=pq.peek();
-            for(int i=0;i<groupSize;i++){
-                if(!pq.contains(head+i)){
+             for(int i=0;i<groupSize;i++){
+                while(!pq.contains(head+i)){
                     return false;
-                }else{
-                    pq.remove(head+i);
                 }
-            }
-        }
+                pq.remove(head+i);
+             }
         }
         return true;
+
     }
 }
