10a9d70a0671d25f9e5e063e64fa8b982d27bfbf
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-06-11 19:54:13 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (61.60%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,28 +1,29 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-         int freq[]=new int[1001];
-         int i=0;
-         int max=0;
+        int i=0;
+        int max=0;
+        int freq[]=new int[1001];
 
-         for(int num:arr1){
-            freq[num]++;//we will count the frequency of the element
+
+        for(int num:arr1){
+            freq[num]++;
             max=Math.max(num,max);
-         }
-         int sorted[]=new int[arr1.length];
-
-         for(int num:arr2){
+        }
+        int sorted[]=new int[arr1.length];
+        for(int num:arr2){
             while(freq[num]-->0){
-                sorted[i]=num;
-                i++;
+            sorted[i]=num;
+            i++;
             }
-         }
-         for(int x=0;x<=max;x++){
-             while(freq[x]-->0){
+        }
+        for(int x=0;x<=max;x++){
+            while(freq[x]-->0){
                 sorted[i]=x;
                 i++;
-             }
-         }
-         return sorted;
+            }
+        }
+        return sorted;
 
+        
     }
 }
