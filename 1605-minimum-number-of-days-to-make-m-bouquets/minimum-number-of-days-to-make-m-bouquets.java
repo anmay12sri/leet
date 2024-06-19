dccbf0c18cd7556bab5f80c92fe5890e0585f515
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-06-19 12:57:02 +0530

    Time: 25 ms (14.27%) | Memory: 58.1 MB (30.44%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,44 +1,42 @@
 class Solution {
-    public int canMakeBouquet(int[] bloomDay, int mid, int k) {
-        int bouquet = 0;
-        int consecutive = 0;
-        for (int i = 0; i < bloomDay.length; i++) {
-            if (bloomDay[i] <= mid) {
+    public int canMakeBouquet(int[]bloomDay,int mid,int k){
+        int bouquet=0;
+        int consecutive=0;
+        for(int i=0;i<bloomDay.length;i++){
+            if(bloomDay[i]<=mid){
                 consecutive++;
-            } else {
-                consecutive = 0;
+            }else{
+                consecutive=0;
             }
-            if (consecutive == k) {
+            if(consecutive==k){
                 bouquet++;
-                consecutive = 0;
+                consecutive=0;
             }
         }
         return bouquet;
     }
-
     public int minDays(int[] bloomDay, int m, int k) {
-        // Impossible to make the required bouquets
-        if (bloomDay == null || bloomDay.length == 0 || m * k > bloomDay.length) {
-            return -1;
+         if (bloomDay == null || bloomDay.length == 0 || m * k > bloomDay.length) {
+            return -1; // Impossible to make the required bouquets
         }
-
-        int start = Integer.MAX_VALUE;
+      int start = Integer.MAX_VALUE;
         int end = Integer.MIN_VALUE;
-        for (int i = 0; i < bloomDay.length; i++) {
-            start = Math.min(bloomDay[i], start);
-            end = Math.max(bloomDay[i], end);
-        }
+       int mindays=-1;
+       for(int i=0;i<bloomDay.length;i++){
+        start=Math.min(bloomDay[i],start);
+        end=Math.max(bloomDay[i],end);
+       }
+       while(start<=end){
+        int mid=start+(end-start)/2;
 
-        int mindays = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (canMakeBouquet(bloomDay, mid, k) >= m) {
-                mindays = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+        if(canMakeBouquet( bloomDay,  mid,k)>=m){
+            mindays=mid;
+            end=mid-1;
+        }else{
+            start=mid+1;
         }
-        return mindays;
+       }
+       return mindays;
+
     }
 }
