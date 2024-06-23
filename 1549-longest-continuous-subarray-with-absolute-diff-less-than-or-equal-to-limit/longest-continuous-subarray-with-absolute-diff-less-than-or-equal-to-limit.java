575c3b49b4bd3e11359b28533b68dd9ccb7103b5
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-06-23 15:41:02 +0530

    Time: 41 ms (46.12%) | Memory: 57.8 MB (17.06%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -3,30 +3,30 @@ class Solution {
         int n=nums.length;
         int i=0;
         int j=0;
-        int maxsub=0;
-        int curmax=nums[0];
         int curmin=nums[0];
+        int curmax=nums[0];
+        int maxsub=0;
         while(j<n){
-            curmax=Math.max(nums[j],curmax);
             curmin=Math.min(nums[j],curmin);
+            curmax=Math.max(nums[j],curmax);
             if(curmax-curmin<=limit){
-                maxsub=Math.max(j-i+1,maxsub);
-                j++;
-            }else{
-                if(nums[i]==curmax || nums[i]==curmin){
-                    curmax=nums[i+1];
-                    curmin=nums[i+1];
-                    for(int k=i+1;k<=j;k++){
-                        curmax=Math.max(nums[k],curmax);
-                        curmin=Math.min(nums[k],curmin);
-                    }
-                    // i++;
-                }
-                i++;
-                j++;
+                maxsub=Math.max(maxsub,j-i+1);
+            j++;
             }
-             
+        else{
+        
+            if(nums[i]==curmin || nums[i]==curmax){
+                curmin=nums[i+1];
+                curmax=nums[i+1];
+                for(int k=i+1;k<=j;k++){
+                    curmin=Math.min(nums[k],curmin);
+                    curmax=Math.max(nums[k],curmax);
+                }
+            }
+            i++;
+            j++;
         }
+    }
         return maxsub;
     }
 }
