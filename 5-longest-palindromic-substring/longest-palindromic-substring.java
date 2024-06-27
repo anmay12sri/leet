bea358613b561e0fe3bccbd8b6e85ec6d49579c9
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-06-27 23:32:18 +0530

    Time: 262 ms (23.99%) | Memory: 58.5 MB (5.09%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,18 +1,25 @@
 class Solution {
+    private Boolean[][]memo;
     public boolean solve(String s,int i,int j){
         if(i>=j){
             return true;
         };
+        if(memo[i][j]!=null){
+            return memo[i][j];
+        }
 
         if(s.charAt(i)==s.charAt(j)){
-            return solve(s,i+1,j-1);
+            memo[i][j]= solve(s,i+1,j-1);
+            return memo[i][j];
         }
+        memo[i][j]=false;
         return false;
     }
     public String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
         int sp=0;
         int max=0;
+        memo =new Boolean[s.length()][s.length()];
 
         for(int i=0;i<s.length();i++){
             for(int j=i;j<s.length();j++){
