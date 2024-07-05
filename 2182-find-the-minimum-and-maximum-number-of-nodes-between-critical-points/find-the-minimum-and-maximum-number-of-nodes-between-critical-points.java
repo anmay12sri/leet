779d4fe541bf351d9e2d5666074e01733ce339f1
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-06 00:52:20 +0530

    Time: 4 ms (100.00%) | Memory: 60.5 MB (54.76%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,31 +10,31 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int arr[]=new int[2];
         ListNode temp=head;
         ListNode curr=head.next;
+        int prev=Integer.MIN_VALUE;
+        int mindist=Integer.MAX_VALUE;
         int i=1;
-        int previ=Integer.MIN_VALUE;
-        int min_dist=Integer.MAX_VALUE;
-        int firsti=-1;
+        int first=-1;
 
         while(curr.next!=null){
-            if((curr.val>temp.val && curr.val> curr.next.val)||(curr.val<temp.val && curr.val<curr.next.val)){
-                 if(previ!=Integer.MIN_VALUE){
-                    min_dist=Math.min(min_dist,i-previ);
-                 }
-                 if(firsti==-1){
-                    firsti=i;
-                 }
-                 previ=i;
+            if((curr.val<temp.val && curr.val<curr.next.val)|| (curr.val>temp.val && curr.val>curr.next.val)){
+                if(prev!=Integer.MIN_VALUE){
+                    mindist=Math.min(mindist,i-prev);
+                }
+                if(first==-1){
+                    first=i;
+                }
+                prev=i;
+                
             }
-            temp=curr;
-            curr=curr.next;
-            i++;
+                temp=curr;
+                curr=curr.next;
+                i++;
+            }
+            if(mindist==Integer.MAX_VALUE){
+                return new int[]{-1,-1};
         }
-        if(min_dist==Integer.MAX_VALUE){
-            return new int[]{-1,-1};
-        }
-        return new int[]{min_dist,previ-firsti};
+        return new int[]{mindist,prev-first};
     }
 }
