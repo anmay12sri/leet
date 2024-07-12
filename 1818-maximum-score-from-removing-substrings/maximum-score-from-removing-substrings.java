1eff7be926290d2292a22794e00df15827aa3be0
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-12 19:07:54 +0530

    Time: 124 ms (58.59%) | Memory: 45.4 MB (76.77%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String removeSubstr(String s,String match){
+        Stack<Character> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (!st.isEmpty() && ch == match.charAt(1) && st.peek() == match.charAt(0)) {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+            StringBuilder sb = new StringBuilder();
+        for (char ch : st) {
+            sb.append(ch);
+        }
+        
+        return sb.toString();
+        
+    }
+    public int maximumGain(String s, int x, int y) {
+        int n=s.length();
+        int score=0;
+        
+        String maxst = (x > y) ? "ab" : "ba";
+        String minst = (x > y) ? "ba" : "ab";
+
+        // Remove the substring that gives the higher score first
+        String temp_f = removeSubstr(s, maxst);
+        int L = temp_f.length();
+        int charrem = n - L;
+        score += (charrem / 2) * Math.max(x, y);
+
+        // Remove the substring that gives the lower score second
+        String temp_s = removeSubstr(temp_f, minst);
+        charrem = L - temp_s.length();
+        score += (charrem / 2) * Math.min(x, y);
+
+        return score;
+
+    }
+}
