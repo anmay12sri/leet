e6234390311cfacffb0355f2bfb5099785f912db
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-13 16:49:21 +0530

    Time: 5 ms (67.74%) | Memory: 57.7 MB (52.91%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -3,13 +3,15 @@ class Solution {
         int l=0;
         int r=height.length-1;
         int max=Integer.MIN_VALUE;
+        // int curr=0;
+
         while(l<r){
-            int cur=Math.min(height[l],height[r])*(r-l);
-              max=Math.max(cur,max);
+             int curr=Math.min(height[l],height[r])*(r-l);
+              max=Math.max(max,curr);
             if(height[l]<height[r]){
                 l++;
             }else{
-                r--;
+            r--;
             }
         }
         return max;
