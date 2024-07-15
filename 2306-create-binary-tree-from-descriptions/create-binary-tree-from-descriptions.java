f673a504dd39455e2b2d2992478da565741f2fd9
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-15 16:20:14 +0530

    Time: 78 ms (25.83%) | Memory: 55.6 MB (67.55%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -16,29 +16,31 @@
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
         Map<Integer,TreeNode>map=new HashMap<>();
-        Set<Integer>finall=new HashSet<>();
+        Set<Integer>set=new HashSet<>();
 
         for(int d[]:descriptions){
-            int par=d[0], child=d[1] ,isLeft=d[2];
+            int par=d[0];
+            int child=d[1];
+            int left=d[2];
+
             if(!map.containsKey(par)){
                 map.put(par,new TreeNode(par));
             }
             if(!map.containsKey(child)){
                 map.put(child,new TreeNode(child));
             }
-            if(isLeft==1){
-              map.get(par).left=map.get(child);
-            }else if(isLeft==0){
+            if(left==1){
+                map.get(par).left=map.get(child);
+            }else{
                 map.get(par).right=map.get(child);
-             }
-             finall.add(child);
-        }
-        for(Map.Entry<Integer,TreeNode>e:map.entrySet()){
-            if(!finall.contains(e.getKey())){
-                return e.getValue();
             }
+            set.add(child);
         }
+            for(Map.Entry<Integer,TreeNode>e:map.entrySet()){
+                if(!set.contains(e.getKey())){
+                    return e.getValue();
+                }
+            }
         return null;
-        
     }
 }
