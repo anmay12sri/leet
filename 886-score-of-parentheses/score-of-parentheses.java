a3bc328d627ecc1c82472ef0a19e779e6ba917de
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-17 23:41:55 +0530

    Time: 1 ms (44.39%) | Memory: 41.1 MB (68.46%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.java
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -2,14 +2,15 @@ class Solution {
     public int scoreOfParentheses(String s) {
         int count=0;
         Stack<Integer>st=new Stack<>();
+
         for(char c:s.toCharArray()){
             if(c=='('){
                 st.push(0);
             }else{
                 int val=0;
-                while(st.peek()!=0)val+=st.pop();//add krta jayega
-                val=Math.max(2*val,1);//for inner loop and if()=>1
-                st.pop();//previous value remove krega
+                while(st.peek()!=0)val+=st.pop();
+                val=Math.max(2*val,1);
+                st.pop();
                 st.push(val);
             }
         }
@@ -17,6 +18,5 @@ class Solution {
             count+=st.pop();
         }
         return count;
-        
     }
 }
