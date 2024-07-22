e2ad80e75d6211235954dd5ec3405251a98c449c
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-22 23:46:11 +0530

    Time: 5 ms (56.90%) | Memory: 44.7 MB (29.14%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -9,15 +9,15 @@ class Solution {
         map.put('D',500);
         map.put('M',1000);
 
-        int res=map.get(s.charAt(s.length()-1));
+         int res=map.get(s.charAt(s.length()-1));
 
-        for(int i=s.length()-2;i>=0;i--){
-        if(map.get(s.charAt(i))< map.get(s.charAt(i+1))){
-           res-=map.get(s.charAt(i)); 
-        }else{
-            res+=map.get(s.charAt(i));
-        }
-        }
-        return res;
+         for(int i=s.length()-2;i>=0;i--){
+            if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){
+                res-=map.get(s.charAt(i));
+            }else{
+                res+=map.get(s.charAt(i));
+            }
+         }
+         return res;
     }
 }
