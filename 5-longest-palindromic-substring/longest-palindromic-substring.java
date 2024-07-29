9fdc1973e0f06f4f4a4bcfbd9d8884536538d2f4
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-29 19:53:33 +0530

    Time: 256 ms (25.61%) | Memory: 58.3 MB (5.14%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,36 +1,36 @@
 class Solution {
-    private Boolean[][]memo;
-    public boolean solve(String s,int i,int j){
-        if(i>=j){
-            return true;
-        };
+    private Boolean memo[][];
+     public boolean solve(String s,int i,int j){
+        if(i>=j)return true;
+
         if(memo[i][j]!=null){
             return memo[i][j];
         }
-
         if(s.charAt(i)==s.charAt(j)){
-            memo[i][j]= solve(s,i+1,j-1);
+            memo[i][j]=solve(s,i+1,j-1);
             return memo[i][j];
         }
         memo[i][j]=false;
         return false;
-    }
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-        int sp=0;
-        int max=0;
-        memo =new Boolean[s.length()][s.length()];
 
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(solve(s,i,j)==true){
-                    if(j-i+1>max){
-                        max=j-i+1;
-                        sp=i;
-                    }
+
+     }
+    public String longestPalindrome(String s) {
+       if(s.length()<1 || s ==null)return "";
+       int sp=0;
+       int max=0;
+       memo=new Boolean[s.length()][s.length()];
+
+       for(int i=0;i<s.length();i++){
+        for(int j=i;j<s.length();j++){
+            if(solve(s,i,j)==true){
+                if(max<j-i+1){
+                    max=j-i+1;
+                    sp=i;
                 }
             }
         }
-        return s.substring(sp,sp+max);
+       }
+       return s.substring(sp,sp+max);
     }
 }
