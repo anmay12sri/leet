ee0c68cfb858192bc20c57ebee5f722a149eba10
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-29 11:46:31 +0530

    Time: 1994 ms (13.79%) | Memory: 43 MB (20.25%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.java
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<rating.length;i++){
             for(int j=i+1;j<rating.length;j++){
                 for(int k=j+1;k<rating.length;k++){
-                    if(rating[i]<rating[j] && rating[j]<rating[k] || rating[i]>rating[j] && rating[j]>rating[k]){
-                        count++;
-                    }
+                   if(((rating[i]<rating[j]) && (rating[j]<rating[k]))||((rating[i]>rating[j])&&(rating[j]>rating[k]))){
+                    count++;
+                   }
                 }
             }
         }
