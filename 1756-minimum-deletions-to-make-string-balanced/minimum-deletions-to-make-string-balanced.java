ab221b37c7a05c475fa58c856d9a557ffa1c5807
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-07-30 22:11:45 +0530

    Time: 22 ms (77.72%) | Memory: 45.5 MB (23.12%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minimumDeletions(String s) {
         int count=0,ans=0;
-         for(int c:s.toCharArray()){
-            if(c=='b'){
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='b'){
                 count++;
             }else if(count>0){
                 ans++;
                 count--;
             }
-         }
-         return ans;
+        }
+        return ans;
     }
 }
