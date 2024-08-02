cf471271cabd4294ce5874c9bfb6ff9dd26d1fa2
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-08-03 00:40:24 +0530

    Time: 2 ms (84.33%) | Memory: 44.8 MB (14.70%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>>list=new ArrayList<>();
         List<Integer>curr=new ArrayList<>();
-        solve(list,curr,candidates,target,0);
+        List<List<Integer>>list=new ArrayList<>();
+        backtrack(list,curr,candidates,target,0);
         return list;
     }
-    public static void solve(List<List<Integer>>list,List<Integer>curr,int[]candidates,int target,int s){
-         if(target<0){
+    public static void backtrack(List<List<Integer>>list,List<Integer>curr,int candidates[],int target,int s){
+        if(target<0){
             return;
-         }
+        }
         if(target==0){
             list.add(new ArrayList<>(curr));
             return;
         }
-        if(s==candidates.length){
-            return;
-        }
         for(int i=s;i<candidates.length;i++){
             curr.add(candidates[i]);
-            solve(list,curr,candidates,target-candidates[i],i);
+            backtrack(list,curr,candidates,target-candidates[i],i);
             curr.remove(curr.size()-1);
         }
+         
     }
 }
