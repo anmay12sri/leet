380279b1481d107227ac51a8cd7351edb3603b76
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-08-03 16:11:14 +0530

    Time: 2 ms (99.65%) | Memory: 43.3 MB (23.58%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -17,6 +17,9 @@ class Solution {
             if(i>s && candidates[i]==candidates[i-1]){
                 continue;
             }
+            if (candidates[i] > target) {
+                break;
+            }
             curr.add(candidates[i]);
             backtrack(list,curr,candidates,target-candidates[i],i+1);
             curr.remove(curr.size()-1);
