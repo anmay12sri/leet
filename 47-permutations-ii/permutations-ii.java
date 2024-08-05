54a9f59e86451ce57218d685d77186843e73e309
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-08-06 02:13:56 +0530

    Time: 583 ms (11.57%) | Memory: 44.9 MB (40.75%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-       List<List<Integer>>list=new ArrayList<>();
-       List<Integer>curr=new ArrayList<>();
-       boolean[]used=new boolean[nums.length];
-       Arrays.sort(nums);
-       backtrack(list,curr,nums,used);
-       return list; 
+        List<List<Integer>>list=new ArrayList<>();
+        List<Integer>curr=new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list,curr,nums,new boolean[nums.length]);
+        return list;
     }
     public static void backtrack(List<List<Integer>>list,List<Integer>curr,int nums[],boolean used[]){
-        if(curr.size()==nums.length){
+         if(curr.size()==nums.length && !list.contains(curr)){
             list.add(new ArrayList<>(curr));
             return;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(used[i]|| i>0 && nums[i]==nums[i-1] && !used[i-1])continue;
+         }
+         for(int i=0;i<nums.length;i++){
+            if(used[i])continue;
             curr.add(nums[i]);
             used[i]=true;
             backtrack(list,curr,nums,used);
             used[i]=false;
             curr.remove(curr.size()-1);
-
-        }
+         }
     }
 }
