dd0c32f2840bcd07418118a217c322529fa31a42
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-08-11 19:30:54 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (21.64%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -13,20 +13,19 @@ class Solution {
         }
         return max;
     }
-        private int dfs(int grid[][], int i, int j){
-            if(i<0 || i>=grid.length || j<0 || j>=grid[0].length|| grid[i][j]==0){
-                return 0;
-            }
-            grid[i][j]=0;
-            int count=1;
-
-
-            count+=dfs(grid,i,j-1);
-            count+=dfs(grid,i,j+1);
-            count+=dfs(grid,i-1,j);
-            count+=dfs(grid,i+1,j);
-
-            return count;
+    public int dfs(int grid[][], int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]==0){
+            return 0;
         }
-    
+        int count=1;
+        
+        grid[i][j]=0;
+
+       count+= dfs(grid,i,j-1);
+        count+=dfs(grid,i,j+1);
+        count+=dfs(grid,i-1,j);
+        count+=dfs(grid,i+1,j);
+
+        return count;
+    }
 }
