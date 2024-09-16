4d95842a694aaa4d6628d9f75a178b72d28d4778
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-09-16 14:32:01 +0530

    Time: 8 ms (48.18%) | Memory: 46.2 MB (26.16%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -1,19 +1,18 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-       int []times=new int[timePoints.size()];
-       int min=Integer.MAX_VALUE;
+        int []times=new int[timePoints.size()];
 
-       for(int i=0;i<times.length;i++){
-        String timearr[]=timePoints.get(i).split(":");
-        times[i]=Integer.parseInt(timearr[0])*60 + Integer.parseInt(timearr[1]);
-       }
-       Arrays.sort(times);
-       for(int i=1;i<times.length;i++){
-        min=Math.min(times[i]-times[i-1],min);
-       }
-       //base case
-       int twentyfour=Math.abs(times[times.length-1]-(times[0]+1440));
-       min=Math.min(min,twentyfour);
-       return min; 
+        for(int i=0;i<times.length;i++){
+            String timearr[]=timePoints.get(i).split(":");
+            times[i]=Integer.parseInt(timearr[0])*60+Integer.parseInt(timearr[1]);
+        }
+        Arrays.sort(times);
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<times.length;i++){
+            min=Math.min(times[i]-times[i-1],min);
+        }
+        int twentyfour=Math.abs(times[times.length-1]-(times[0]+1440));
+        min=Math.min(min,twentyfour);
+        return min;
     }
 }
