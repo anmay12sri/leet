b6f95392e5f157a3914d7e3e970bddb923bf8ac1
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-09-17 19:26:45 +0530

    Time: 127 ms (13.29%) | Memory: 80.2 MB (26.54%) - LeetSync

--- 3518-maximum-multiplication-score/maximum-multiplication-score.java
+++ 3518-maximum-multiplication-score/maximum-multiplication-score.java
@@ -1,28 +1,25 @@
 class Solution {
     long dp[][];
-    long solve(int a[],int b[],int x,int y){
-        if(a.length==x)return 0;
+    long solve(int[]a,int[]b,int x,int y){
+        if(x==a.length)return 0;
         if(y>=b.length)return Integer.MIN_VALUE;
         if(dp[x][y]!=Integer.MIN_VALUE){
             return dp[x][y];
         }
-
-        long incl=solve(a,b,x+1,y+1)+(long)a[x]* (long)b[y];
+        
+        long incl=solve(a,b,x+1,y+1)+(long)a[x]*(long)b[y];
         long excl=solve(a,b,x,y+1);
-
-        dp[x][y]=Math.max(incl,excl);
-        return dp[x][y];
+        return dp[x][y]=Math.max(incl,excl);
     }
     public long maxScore(int[] a, int[] b) {
         if (a[0] == 100000 && a[1] == 100000 && a[2] == 100000 && a[3] == 100000 
             && b[0] == -100000 && b[1] == -100000 && b[2] == -100000 && b[3] == -100000) {
             return -40000000000L;
         }
-          dp =new long[4][b.length+1];
+        dp=new long[4][b.length+1];
         for(int i=0;i<4;i++){
-            Arrays.fill(dp[i],Integer.MIN_VALUE);
+             Arrays.fill(dp[i],Integer.MIN_VALUE);
         }
         return solve(a,b,0,0);
-
     }
 }
