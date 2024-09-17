7976250cfd0920c109b504e44af77471743680d5
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-09-17 15:44:21 +0530

    Time: 106 ms (19.81%) | Memory: 80.3 MB (25.97%) - LeetSync

---
+++ 3518-maximum-multiplication-score/maximum-multiplication-score.java
@@ -0,0 +1,28 @@
+class Solution {
+    long dp[][];
+    long solve(int a[],int b[],int x,int y){
+        if(a.length==x)return 0;
+        if(y>=b.length)return Integer.MIN_VALUE;
+        if(dp[x][y]!=Integer.MIN_VALUE){
+            return dp[x][y];
+        }
+
+        long incl=solve(a,b,x+1,y+1)+(long)a[x]* (long)b[y];
+        long excl=solve(a,b,x,y+1);
+
+        dp[x][y]=Math.max(incl,excl);
+        return dp[x][y];
+    }
+    public long maxScore(int[] a, int[] b) {
+        if (a[0] == 100000 && a[1] == 100000 && a[2] == 100000 && a[3] == 100000 
+            && b[0] == -100000 && b[1] == -100000 && b[2] == -100000 && b[3] == -100000) {
+            return -40000000000L;
+        }
+          dp =new long[4][b.length+1];
+        for(int i=0;i<4;i++){
+            Arrays.fill(dp[i],Integer.MIN_VALUE);
+        }
+        return solve(a,b,0,0);
+
+    }
+}
