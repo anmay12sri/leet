c2a4a587d6fa8a4a734a8c257298829000a93363
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-09-18 17:31:27 +0530

    Time: 5 ms (98.24%) | Memory: 43 MB (65.61%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -6,13 +6,14 @@ class Solution {
             elements[i]=Integer.toString(nums[i]);
         }
 
-        Arrays.sort(elements,new Comparator<String>(){
-            public int compare(String a,String b){
-               String first=a+b;
-               String sec=b+a;
-               return sec.compareTo(first); 
-            }
-        });
+        // Arrays.sort(elements,new Comparator<String>(){
+        //     public int compare(String a,String b){
+        //        String first=a+b;
+        //        String sec=b+a;
+        //        return sec.compareTo(first); 
+        //     }
+        // });
+        Arrays.sort(elements,(a,b)->(b+a).compareTo(a+b));
         //special case
         if(elements[0].equals("0")){
             return "0";
