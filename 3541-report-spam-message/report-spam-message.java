2d54b422bd84b8fd3dbb49b0bf475abc43eb2ced
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-09-22 14:07:06 +0530

    Time: 51 ms (100.00%) | Memory: 74 MB (100.00%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean reportSpam(String[] message, String[] bannedWords) {
+        
+        Set<String>banned1=new HashSet<>();// for fast lookup
+
+        for(String x:bannedWords){
+            banned1.add(x);
+        }
+        int count=0;
+
+        for(String msg:message){
+            if(banned1.contains(msg)){
+                count++;
+            }
+            if(count>=2){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
