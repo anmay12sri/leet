6bcf15f8552de680b934d903080ecd0d1106baad
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-09-23 15:24:53 +0530

    Time: 46 ms (42.18%) | Memory: 45.3 MB (36.39%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -1,15 +1,13 @@
 class Solution {
-    
     public int minExtraChar(String s, String[] dictionary) {
         int n=s.length();
         Set<String>set=new HashSet<>();
+        int memo[]=new int[n];
+        Arrays.fill(memo,-1);
         for(String st:dictionary){
             set.add(st);
         }
-        int memo[]=new int[s.length()];
-        Arrays.fill(memo,-1);
         return solve(s,set,0,memo);
-        
     }
     public int solve(String s,Set<String>set,int i,int memo[]){
         if(i==s.length()){
@@ -20,10 +18,10 @@ class Solution {
         }
         int minExt=1+solve(s,set,i+1,memo);
 
-        for(int end=i;end<s.length();end++){
-            String curr=s.substring(i,end+1);
+        for(int j=i;j<s.length();j++){
+            String curr=s.substring(i,j+1);
             if(set.contains(curr)){
-                minExt=Math.min(minExt,solve(s,set,end+1,memo));
+            minExt=Math.min(minExt,solve(s,set,j+1,memo));
             }
         }
         return memo[i]=minExt;
