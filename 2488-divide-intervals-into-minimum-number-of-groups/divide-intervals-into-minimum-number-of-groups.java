8aecf94420d2a95268eeea2fb5ef5805877159f9
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-10-12 12:08:31 +0530

    Time: 25 ms (97.55%) | Memory: 94.7 MB (15.49%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
@@ -1,27 +1,24 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-      //find range
-      int n=intervals.length;
-      int min=Integer.MAX_VALUE;
-      int max=Integer.MIN_VALUE;
+       int n=intervals.length;
+       int min=Integer.MAX_VALUE;
+       int max=Integer.MIN_VALUE;
 
-      for(int i=0;i<n;i++){
+       for(int i=0;i<n;i++){
         min=Math.min(min,intervals[i][0]);
         max=Math.max(max,intervals[i][1]);
-
-      }
-      int eventcount[]=new int[max+2];
-
-      for(int i=0;i<n;i++){
-        eventcount[intervals[i][0]]++;
-        eventcount[intervals[i][1] + 1]--;
-      }
-      int maxoverlaps=0;
-      int sum=0;
-      for(int i=min;i<max+2;i++){
-        sum+=eventcount[i];
-        maxoverlaps=Math.max(maxoverlaps,sum);
-      }
-      return maxoverlaps;
+       } 
+       int evencounts[]=new int[max+2];
+       for(int i=0;i<n;i++){
+        evencounts[intervals[i][0]]++;
+        evencounts[intervals[i][1]+1]--;
+       }
+       int maxoverlapping=0;
+       int sum=0;
+       for(int i=min;i<max+2;i++){
+        sum+=evencounts[i];
+        maxoverlapping=Math.max(maxoverlapping,sum);
+       }
+       return maxoverlapping;
     }
 }
