83d20e647ec8041294b4197efef56f9f5e4f5c5a
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-10-15 09:48:24 +0530

    Time: 7 ms (100.00%) | Memory: 45.7 MB (20.43%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,18 +1,13 @@
 class Solution {
     public long minimumSteps(String s) {
-        int n=s.length();
+        long res=0;
         int last=0;
-        long ans=0;
-        
-        for (int i=0;i<n;i++)
-        {
-            if (s.charAt(i)=='0')
-            {
-                ans+=(i-last);
-                last++;
-            }
+        for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='0'){
+          res+=(i-last);
+          last++;
         }
-        
-        return ans;
+        }
+        return res;
     }
 }
