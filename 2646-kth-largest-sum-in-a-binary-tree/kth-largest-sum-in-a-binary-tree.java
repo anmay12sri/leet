113b2ef1197bf7c865186f2d7804bcda0e2ca22e
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-10-22 22:57:13 +0530

    Time: 39 ms (33.76%) | Memory: 62.6 MB (66.81%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
@@ -15,35 +15,35 @@
  */
 class Solution {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        if(root==null)return -1;
         Queue<TreeNode>q=new LinkedList<>();
-        q.add(root);
-
+        q.offer(root);
         List<Long>levelsum=new ArrayList<>();
-        // levelsum.add(root.val);
+
+        if(root==null)return -1;
 
         while(!q.isEmpty()){
-          int levelsize=q.size();
-          long sum=0;
+            int levelsize=q.size();
 
-          for(int i=0;i<levelsize;i++){
-            TreeNode curr=q.poll();
-            sum+=curr.val;
+            long sum=0;
 
-            if(curr.left!=null){
-                q.add(curr.left);
+            for(int i=0;i<levelsize;i++){
+                TreeNode curr=q.poll();
+                sum+=curr.val;
+
+                if(curr.left!=null){
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    q.offer(curr.right);
+                }
             }
-            if(curr.right!=null){
-                q.add(curr.right);
-            }
-          }
-          levelsum.add(sum);
+            levelsum.add(sum);
+             
         }
         Collections.sort(levelsum,Collections.reverseOrder());
-
-        if(k<=levelsum.size()){
-            return levelsum.get(k-1);
-        }
-        return -1;
+            if(k<=levelsum.size()){
+                return levelsum.get(k-1);
+            }
+            return -1;
     }
 }
