e1994ac2f1f98c8b58b4961a648f75bdc1920fb2
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-10-28 22:28:50 +0530

    Time: 51 ms (53.13%) | Memory: 63.3 MB (9.03%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
@@ -1,26 +1,23 @@
 class Solution {
     public int longestSquareStreak(int[] nums) {
-       Set<Long>set=new HashSet<>();
-
-       for(long num:nums){
-        set.add(num);
-       }
-       int longestcount=0;
-       for(long num:nums){
-        long curr=num;
-        int count=0;
-
-        while(set.contains(curr)){
-            count++;
-            long next=curr*curr;
-        if(next<0 || !set.contains(next)){
-            break;
+        Set<Long>set=new HashSet<>();
+        int longcount=0;
+        for(long num:nums){
+            set.add(num);
         }
-          curr=next;
+        for(long num:nums){
+             long curr=num;
+             int count=0;
+             while(set.contains(curr)){
+               count++;
+               long next=curr*curr;
+               if(next<0 ||!set.contains(next)){
+                break;
+               }
+               curr=next;
+             }
+             longcount=Math.max(count,longcount);
         }
-       
-       longestcount=Math.max(count,longestcount);
-       }
-       return longestcount>1?longestcount:-1; 
+        return longcount>1?longcount:-1;
     }
 }
